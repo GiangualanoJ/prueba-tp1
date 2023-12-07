174c6bd1157f037e4e8134c709f920a14611be44
Josefina <giangualanoj@gmail.com>
2023-12-07 16:44:36 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const dbConnection = new Sequelize({
     dialect: 'mysql',
     host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
+    username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     port: process.env.DATABASE_PORT,
